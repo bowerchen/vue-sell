2ea1439acf64b8b25ee4bda8c423807da27b6acd
bowerchen <someboy1997@outlook.com>
2019-03-18 11:47:41 +0800

    Node服务器修改

--- prod.server.js
+++ prod.server.js
@@ -1,56 +1,55 @@
-var express = require('express')
-var config = require('./config/index')
+var express = require('express');
+var config = require('./config/index');
 
-var port = process.env.PORT || config.build.port
+var port = process.env.PORT || config.build.port;
 
-// var port = process.env.PORT || 8898
+var app = express();
 
-var app = express()
+var router = express.Router();
 
-var router = express.Router()
+router.get('/', function (req, res, next) {
+	req.url = '/index.html';
+	next();
+});
 
-router.get('/', function(req, res, next) {
-    req.url = '/index.html'
-    next()
-})
+app.use(router);
 
-app.use(router)
+var appData = require('./data.json');
+var seller = appData.seller;
+var goods = appData.goods;
+var ratings = appData.ratings;
 
-var appDate = require('./data.json')
-var seller = appDate.seller
-var goods = appDate.goods
-var ratings = appDate.ratings
+var apiRoutes = express.Router();
 
-var apiRoutes = express.Router()
+apiRoutes.get('/seller', function (req, res) {
+	res.json({
+		errno: 0,
+		data: seller
+	});
+});
 
-apiRoutes.get('/seller', function(req, res) {
-    res.json({
-        errno: 0,
-        data: seller
-    })
-})
+apiRoutes.get('/goods', function (req, res) {
+	res.json({
+		errno: 0,
+		data: goods
+	});
+});
 
-apiRoutes.get('/goods', function(req, res) {
-    res.json({
-        errno: 0,
-        data: goods
-    })
-})
+apiRoutes.get('/ratings', function (req, res) {
+	res.json({
+		errno: 0,
+		data: ratings
+	});
+}); 
 
-apiRoutes.get('/ratings', function(req, res) {
-    res.json({
-        errno: 0,
-        data: ratings
-    })
-})
+app.use('/api', apiRoutes);
 
-app.use('/api', apiRoutes)
-app.use(express.static('./dist'))
+app.use(express.static('./dist'));
 
-module.exports = app.listen(port, function(err) {
-    if (err) {
-        console.log(err)
-        return
-    }
-    console.log('Listening at http://localhost' + port + '\n')
-})
+module.exports = app.listen(port, function (err) {
+	if (err) {
+		console.log(err);
+		return
+	}
+	console.log('Listening at http://localhost:' + port + '\n')
+});
